32e2a7ae828f4a51ca569f7d8ad5dceaf8b5a188
jiangwenbin <wenbin.jiang@coollu.com.cn>
2022-09-06 13:58:21 +0800

    fix: 改为https仓库

--- deploy.config.example.js
+++ deploy.config.example.js
@@ -4,7 +4,7 @@ module.exports = {
        "user" : "部署服务器的用户名",
        "host" : ["部署服务器的IP"],
        "ref"  : "仓库分支，例如origin/main",
-       "repo" : "git@github.com/cumt-robin/express-blog-backend.git",
+       "repo" : "https://github.com/cumt-robin/express-blog-backend.git",
        "path" : "部署服务器路径，比如/home/xxx/backend/express-blog-backend",
        "post-setup": "npm install -g pm2 && npm install && pm2 start ecosystem.config.js --env production",
        "post-deploy": "git pull && npm install && pm2 restart blog"
