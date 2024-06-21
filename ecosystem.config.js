8043b83d1bd794700cbd0182248f08e3146366dd
cumt-robin <1148121254@qq.com>
2024-06-21 18:50:55 +0800

    chore: 修改pm2配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,27 @@
-// 适用于 docker dev 环境
+const deployConfig = require("./deploy.config")
+
 module.exports = {
-    "apps": [
+    /**
+    * Application configuration section
+    * http://pm2.keymetrics.io/docs/usage/application-declaration/
+    */
+    apps: [
+        // First application
         {
-            "name": "blog",
-            "script": "app.js",
-            "env": {
-                "NODE_ENV": "development",
-                "PORT": 8002
+            // 应用名
+            name: 'blog',
+            // 启动脚本
+            script: 'app.js',
+            // –env参数指定运行的环境
+            env_production: {
+                NODE_ENV: 'production',
+                PORT: 8002,
             },
-            "watch": true,
-            "ignore_watch": ["node_modules", ".git", ".github", ".gitignore", ".dockerignore", ".release-it.cjs", "*.md"],
-            "watch_options": {
-                "usePolling": true
-            }
         }
-    ]
-}
+    ],
+    /**
+    * Deployment section
+    * http://pm2.keymetrics.io/docs/usage/deployment/
+    */
+    deploy: deployConfig
+};
